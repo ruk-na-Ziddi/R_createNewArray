2a0df7d8b4f41a407c11ebe8012e285d12697189
Ankur Kumar Singh <aksinghmt@gmail.com>
2016-07-25 14:58:21 +0530

    added code and test for createNewArray_creates_an_array_of_given_size

--- createNewArray.js
+++ createNewArray.js
@@ -1,6 +1,8 @@
 var r = {};
 exports.r = r;
 
-r.createNewArray = function(){
-	return [];
+r.createNewArray = function(size){
+	var collection = [];
+	size && (collection.length = size)
+	return collection;
 }
--- createNewArrayTest.js
+++ createNewArrayTest.js
@@ -6,3 +6,9 @@ exports.test = test;
 test.createNewArray_creates_an_empty_array = function(){
 	assert.deepEqual(r.createNewArray(),[]);
 };
+
+test.createNewArray_creates_an_array_of_given_size = function(){
+	var a = r.createNewArray(5);
+	assert.deepEqual(a.length,5);
+	assert.equal(a[4],undefined);
+};
